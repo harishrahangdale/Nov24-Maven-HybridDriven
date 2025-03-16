90a1d467fa89847792019863983a43a053d4937f
Harish Rahangdale <hsr.29978@gmail.com>
2025-03-16 11:30:46 +0530

    Add TestConfigs.java with environment variable support

---
+++ src/main/java/base/TestConfigs.java
@@ -0,0 +1,8 @@
+package base;
+
+public class TestConfigs {
+    public static final String VALID_USERNAME = System.getenv("VALID_USERNAME");
+    public static final String VALID_PASSWORD = System.getenv("VALID_PASSWORD");
+    public static final String BASE_URL = "https://staging.app.hirecorrecto.com";
+    public static final String EXPECTED_DASHBOARD_URL = "https://staging.app.hirecorrecto.com/dashboard";
+}
